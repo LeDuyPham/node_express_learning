935b73e02677a386942e53c0c48b3a3b7217a309
Duy.laptop <le.pham@outlook.de>
2024-01-16 15:20:19 +0100

    5:53 im Video drin

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,25 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const{products} = require('./data.js')
+
+app.get('/',(req,res)=>{
+    res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products/:productID',(req,res)=>{
+    //console.log(req)
+    //console.log(req.params)
+const {productID} = req.params;
+
+    const singleProduct = products.find(
+        (product)=> product.id === Number(productID)
+    )
+    if(!singleProduct){
+        return res.status(404).send('Product does not exit')
+    }
+    res.json(singleProduct)
+})
+
+app.listen(5000,()=>{
+    console.log('Server is listening on Port 5000')
+})
